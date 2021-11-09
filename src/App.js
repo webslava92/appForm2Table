d61a1d4ba11ef7d0651b0ecb55512d5d9613d873
Vyacheslav Pustovit <pustovit.slava@gmail.com>
2021-11-09 12:04:39 +0300

    optim

--- src/App.js
+++ src/App.js
@@ -1,141 +1,138 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
 
-
-function App() {
-
-  const [state, setState] = useState({
-      username: '',
-      email: '',
-      items: []
+function App(input, setInput) {
+  const removeUserData = (id) => {
+    const reducedItems = input.items.filter((item, itemId) => {
+      return itemId !== id;
     });
+    setInput({
+      items: reducedItems,
+      id: id,
+      username: input.username,
+      email: input.email,
+    });
+  };
 
+  return (
+    <div className="App">
+      <Form input={input} />
+      <Users removeUserData={removeUserData} />
+    </div>
+  );
+}
+
+function Form() {
+  const [input, setInput] = useState({
+    id: nanoid(),
+    username: "",
+    email: "",
+    items: [],
+  });
   const [stateError, setStateError] = useState({
-      userNameError: '',
-      emailError: ''
+    userNameError: "",
+    emailError: "",
   });
 
-
   let validate = () => {
-    let userNameError = '';
-    let  emailError = '';
-    if (!state.username) {
-      userNameError = 'Name cannot be blank';
-    };
-    if (!state.email.includes('@')) {
-      emailError = 'Incorrect Email';
-    };
+    let userNameError = "";
+    let emailError = "";
+    if (!input.username) {
+      userNameError = "Name cannot be blank";
+    }
+    if (!input.email.includes("@")) {
+      emailError = "Incorrect Email";
+    }
     if (userNameError || emailError) {
-      setStateError({userNameError, emailError});
+      setStateError({ userNameError, emailError });
       return false;
-      
-    };
+    }
     return true;
   };
 
   const formSubmit = (e) => {
-  e.preventDefault();
-  const isValid = validate();
+    e.preventDefault();
+    const isValid = validate();
     if (isValid) {
-      let newItems = [...state.items, {
-        username: state.username, 
-        email: state.email
-      }]
-      setState({
-        items: newItems,
-        username: '',
-        email: ''
-      });
-      setStateError({
-        userNameError: '',
-        emailError: ''
-      })
-    };
-    
+      let newItems = [
+        ...input.items,
+        { id: input.id, username: input.username, email: input.email },
+      ];
+      setInput({ items: newItems, id: nanoid(), username: "", email: "" });
+      setStateError({ userNameError: "", emailError: "" });
+    }
   };
 
   const inputChange = (e) => {
     let { name, value } = e.target;
-    setState(prevState => ({
-      ...prevState,
-      [name]: value 
-    }));
-    
+    setInput((prevInput) => ({ ...prevInput, [name]: value }));
   };
 
-  const removeUserData = index => {
-		const reducedItems = state.items.filter((item, itemIndex) => {
-      return itemIndex !== index
-    });
-    setState({
-      items: reducedItems,
-      username: '',
-      email: ''
-    })
-	}
+  return (
+    <div id="Form">
+      <h3>Add user data:</h3>
+      <form onSubmit={formSubmit}>
+        <input
+          value={input.username}
+          type="text"
+          name="username"
+          onChange={inputChange}
+          placeholder="Name"
+        />
+        <div className="ErrorMessage">{stateError.userNameError}</div>
+        <input
+          value={input.email}
+          type="email"
+          name="email"
+          onChange={inputChange}
+          placeholder="Email"
+        />
+        <div className="ErrorMessage">{stateError.emailError}</div>
+        <button type="submit" value="Submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
 
-    return (
-      <div className="App">
-        <Form formSubmit={ formSubmit } 
-        inputChange={ inputChange } 
-        newUsername={ state.username } 
-        newEmail={ state.email }
-        userNameErrorValue={ stateError.userNameError }
-        emailErrorValue={ stateError.emailError } />
-        
-        <Table items={ state.items } 
-          removeUserData={ removeUserData } />
-      </div>
-    );
-};
+function Users() {
+  return (
+    <div className="Users">
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <User />
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
+function User(input, removeUserData) {
+  const [editId, setEditId] = useState(null);
 
-
-function Table(props) {
-    let {items} = props
-    
-    return (
-      <div className="Table">
-        <table>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Del</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.username}</td>
-                  <td>{item.email}</td>
-                  <td>
-                    <button onClick={() => {props.removeUserData(index)}}>X</button>
-                  </td>
-                </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  };
-
-function Form(props) {
-    return (
-      <div id="Form">
-        <h3>Add user data:</h3>  
-        <form onSubmit={props.formSubmit}>
-          <input value={props.newUsername} type="text" name="username" onChange={props.inputChange} placeholder="Name" />
-          <div className="ErrorMessage">{props.userNameErrorValue}</div>
-          <input value={props.newEmail} type="email" name="email" onChange={props.inputChange} placeholder="Email" />
-          <div className="ErrorMessage">{props.emailErrorValue}</div>
-          <button type="submit" value="Submit">Send</button>
-          
-        </form>
-      </div>
-    );
-};
-
-
+  return input.items.map((item, id) => (
+    <tr key={id}>
+      <td>{item.username}</td>
+      <td>{item.email}</td>
+      <td>
+        <button className="EditBtn" onClick={() => setEditId(item.id)}>
+          Edit
+        </button>
+        <button className="RemoveBtn" onClick={() => removeUserData(id)}>
+          Remove
+        </button>
+      </td>
+    </tr>
+  ));
+}
 
 export default App;
